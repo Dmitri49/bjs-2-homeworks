814008b337e2669216ac7747c8c123026786b516
Dmitri49 <dmitrilissin@gmail.com>
2021-11-04 16:40:26 +0300

    arrays commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);;
 
   return resultArr; // array
 }
