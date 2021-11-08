6193413abb6eae2631c565b2d68f164b3ded86b1
Dmitri49 <dmitrilissin@gmail.com>
2021-11-08 17:08:14 +0300

    objects commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
     // Ваш код
-
 }
 
-Student.prototype.setSubject = function (subjectName) {
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
   //ваш код
 }
 
-// ваш код для остальных методов
+
+Student.prototype.addMark = function(mark) {
+  if ( this.marks === undefined) {
+    this.marks = [];
+  }
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...marks) {
+  for (let i =0; i < marks.length; i++) {
+    this.marks.push(marks[i]);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sum =+ marks[i];
+  }
+
+  return Math.floor(sum / marks.length);
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.reason = reason;
+}
+
+
+
